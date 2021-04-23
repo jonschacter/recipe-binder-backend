f4a7cb5fb5efbc59ece5585604188d2a3547e893
github username <schacter.jon@gmail.com>
2021-04-23 15:03:11 -0700

    add users_controller#update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,32 @@ class UsersController < ApplicationController
         end
     end
 
+    def update
+        user = User.find_by(id: params[:id])
+        if user
+            if user == current_user
+                if user.update(binder_name: user_params[:binder_name])
+                    render json: user, serializer: UserSerializer
+                else
+                    render :json => {
+                        error: user.errors.full_messages.to_sentence
+                    }
+                end
+            else
+                render :json => {
+                    error: "You are not authorized to edit that"
+                }
+            end
+        else
+            render :json => {
+                error: "User could not be found"
+            }
+        end
+    end
+
     private
 
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:username, :password, :binder_name)
     end
 end
