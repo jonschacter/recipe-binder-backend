dba1472346917b99b321307bf47e33b48afb4854
github username <schacter.jon@gmail.com>
2021-04-23 11:31:42 -0700

    configure cookies

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,8 @@ module RecipeBinderBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_recipebinder_session'
   end
 end
