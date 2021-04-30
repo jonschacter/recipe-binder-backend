e2ccd1a28fdae14ff9aa737463db568ca48114b1
github username <schacter.jon@gmail.com>
2021-04-30 12:48:53 -0700

    create Category model with name (str) and position (int)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_183952) do
+ActiveRecord::Schema.define(version: 2021_04_30_194630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "position"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
